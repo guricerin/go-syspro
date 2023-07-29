0a73469970152408d8d7d6a1ef14f182f21e1545
guricerin <chanbo1e9@gmail.com>
2023-07-29 13:22:55 +0900

    p.221

--- ch11/02-tish/main.go
+++ ch11/02-tish/main.go
@@ -4,21 +4,48 @@ import (
 	"errors"
 	"io"
 	"log"
+	"strings"
 
+	"github.com/google/shlex"
 	"github.com/peterh/liner"
 )
+
+func parseCmd(cmdStr string) (cmd string, args []string, err error) {
+	l := shlex.NewLexer(strings.NewReader(cmdStr))
+	cmd, err = l.Next()
+	if err != nil {
+		return
+	}
+	for {
+		token, err := l.Next()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				err = nil
+			}
+			break
+		}
+
+		args = append(args, token)
+	}
+	return
+}
 
 func main() {
 	line := liner.NewLiner()
 	line.SetCtrlCAborts(true)
 
 	for {
-		if cmd, err := line.Prompt("> "); err == nil {
-			if cmd == "" {
+		if cmdline, err := line.Prompt("> "); err == nil {
+			if cmdline == "" {
 				continue
 			}
 			// コマンド処理
-			log.Print(cmd)
+			cmd, args, err := parseCmd(cmdline)
+			if err != nil {
+				log.Print("Error lex line: ", err)
+				continue
+			}
+			log.Print(cmd, args)
 		} else if errors.Is(err, io.EOF) {
 			break
 		} else if err == liner.ErrPromptAborted {
--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/s2a-go v0.1.4 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/google/wire v0.5.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.5 // indirect
